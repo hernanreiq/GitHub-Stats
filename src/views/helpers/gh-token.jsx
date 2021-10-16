b2bbe0a87f85b955324c22179a3e6f9bbd3e4288
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-16 04:19:08 -0400

    Se ha eliminado gh-pages

---
+++ src/views/helpers/gh-token.jsx
@@ -0,0 +1,6 @@
+export const GH_TOKEN = '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';
+/* 
+    GitHub prohibits pushing a personal access token in a public repository, 
+    so the token you are seeing has already been automatically deactivated by GitHub, 
+    that is why you must put your own token if you want to use this application on your localhost.
+*/
