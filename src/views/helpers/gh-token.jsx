aeda56a9fd9035dd47ac1450c926f51b98a52ab8
Reiq.Laptop <reiqvideos@gmail.com>
2022-01-06 23:31:53 -0400

    Token updated

--- src/views/helpers/gh-token.jsx
+++ src/views/helpers/gh-token.jsx
@@ -1,4 +1,15 @@
-export const GH_TOKEN = Buffer.from("Z2hwX2RCRXNaeXRSQ0xCZVZjVnpEVDlwWkFxNElnQlVxODNzOE5HdA==", 'base64').toString('ascii');
+export const GH_TOKEN = convertToken('Z2hwX1RlQU95MThpaXVvSFF4UllOUzcyazRvV1dYaW5FZTBmSkFybw==', 'token');
+
+function convertToken(token, typeBuffer) {
+    var result = '';
+    if(typeBuffer === 'base64'){
+        result = Buffer.from(token).toString('base64');
+    } else if (typeBuffer === 'token'){
+        result = Buffer.from(token, 'base64').toString('ascii');
+    }
+    return result;
+}
+
 /* 
     GitHub prohibits pushing a personal access token in a public repository, 
     so the token you are seeing has already been automatically deactivated by GitHub, 
